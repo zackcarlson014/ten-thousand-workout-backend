74aab1468221edca837b9452818288ffb3b8cda8
zackcarlson014 <jzcarlson014@gmail.com>
2021-06-18 09:07:13 -0500

    resources created

---
+++ db/migrate/20210604021837_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :firstname
+      t.string :lastname
+      t.integer :height
+      t.integer :weight
+      t.integer :age
+      t.integer :bodyfat
+      t.iteger :bmi
+
+      t.timestamps
+    end
+  end
+end
