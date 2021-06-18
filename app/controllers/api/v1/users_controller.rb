67da7f4d8a1e7ebe50ee836a0dfcedf7cc105e66
zackcarlson014 <jzcarlson014@gmail.com>
2021-06-18 12:16:10 -0500

    users controller updated to accomodate password_digest attribute

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,41 +1,43 @@
 class UsersController < ApplicationController
 
+  wrap_parameters :user, include: [:username, :password, :bio, :prof_pic_url]
+
   def index
-    users = User.all
-    render json: users, except: [:updated_at]
+      users = User.all
+      render json: users, except: [:password_digest, :updated_at]
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user, except: [:updated_at]
+      user = User.find(params[:id])
+      render json: user, except: [:password_digest, :updated_at]
   end
 
   def create
-    user = User.create!(user_params)
-    render json: user
+      user = User.create!(user_params)
+      render json: user
   end
 
   def edit
-    user = User.find(params[:id])
-    render json: user
+      user = User.find(params[:id])
+      render json: user
   end
 
   def update
-    user = User.find(params[:id])
-    user.update!(user_params)
-    render json: user, except: [:created_at, :updated_at]
+      user = User.find(params[:id])
+      user.update!(user_params)
+      render json: user, except: [:password_digest, :created_at, :updated_at]
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: user, except: [:created_at, :updated_at]
+      user = User.find(params[:id])
+      user.destroy
+      render json: user, except: [:password_digest, :created_at, :updated_at]
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :firstname, :lastname, :height, :weight, :age, :bodyfat, :bmi)
+    params.require(:user).permit(:username, :firstname, :lastname, :height, :weight, :age, :bodyfat, :bmi, :password)
   end
 
 end
