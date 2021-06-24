d19ccbbc2a9c86be1c962beec679ec8756eba6df
zackcarlson014 <jzcarlson014@gmail.com>
2021-06-24 12:09:25 -0500

    created user reference on workouts migration, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_164714) do
+ActiveRecord::Schema.define(version: 2021_06_24_170728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_164714) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
 end
